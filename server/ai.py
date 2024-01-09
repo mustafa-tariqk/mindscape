8090f324471b861725358c9505b25ebf1f9a7e5b
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-01-09 14:38:29 -0500

    making more of the api calls

--- server/ai.py
+++ server/ai.py
@@ -1,3 +1,27 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import HuggingFacePipeline
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
+base_model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", trust_remote_code=True)
+pipe = pipeline("text-generation", model=base_model, tokenizer=tokenizer)
+llm = HuggingFacePipeline(pipeline=pipe)
+
+
+template = """respond to the instruction below. behave like a chatbot 
+and respond to the user. try to be helpful.
+### Instruction:
+{instruction}
+Answer:"""
+prompt = PromptTemplate(template=template, input_variables=["instruction"])
+
+llm_chain = LLMChain(prompt=prompt,
+                     llm=llm,
+                     )
+question = "INTRODUCE YOURSELF"
+print(llm_chain.run(question))
+
 def ai_message(chat):
     output = "I am a robot"
     return output
--- server/app.py
+++ server/app.py
@@ -11,21 +11,43 @@ def index():
     return "Hello world"
 
 
+def start_chat(user_id):
+    chat = models.Chats(user=user_id, flag=True)
+    models.db.session.add(chat)
+    models.db.session.commit()
+
 """
 @id is the chat id
 @message is the message sent by the user
 """
 @app.route('/converse/<id>/<message>')
-def converse(id, message):
-    ai_text = ai_message(id, message)
+def converse(chat_id, message):
+    ai_text = ai_message(chat_id, message)
 
-    human = models.Messages(chat=id, chat_type='Human', text=message, time=datetime.now())
-    ai = models.Messages(chat=id, chat_type='AI', text=ai_text, time=datetime.now())
+    human = models.Messages(chat=chat_id, chat_type='Human', text=message, time=datetime.now())
+    ai = models.Messages(chat=chat_id, chat_type='AI', text=ai_text, time=datetime.now())
 
     models.db.session.add(human)
     models.db.session.add(ai)
     models.db.session.commit()
     return ai_text
+
+
+"""
+Flags a chat for review
+"""
+@app.route('/flag/<chat_id>')
+def flag_chat(chat_id):
+    chat = models.Chats.query.get(chat_id)
+    chat.flag = True
+    models.db.session.commit()
+
+
+
+def get_all_chats():
+    chats = models.Chats.query.all()
+    
+    return chats
 
 
 
