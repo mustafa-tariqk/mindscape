6d5c208e0017b0bbde891b779177e2170eaa6073
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-02-09 14:26:18 -0500

    done?

--- server/ai.py
+++ server/ai.py
@@ -9,7 +9,7 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_openai import OpenAI
 
-import models
+from models import Messages
 
 load_dotenv()
 llm = OpenAI()
@@ -18,19 +18,28 @@ prompt = PromptTemplate(
     template=open("data/template.txt").read()
 )
 
-
 def ai_message(chat_id, message):
     """
     @chat_id: the id of the chat
     @message: the message to the AI
     @return: the response from the AI
     """
-    session = models.Chats.query.get(chat_id)
+    # Collect message history
+    messages = Messages.query.filter_by(chat=chat_id)
+                             .order_by(Messages.time)
+                             .all()
 
+    # Transform data for langchain useage
     chat_memory = BaseChatMessageHistory()
+    for message in messages:
+        if message.chat_type == "AI":
+            chat_memory.add_ai_message(message.text)
+        else:
+            chat_memory.add_user_message(message.text)
 
+    # Generate output
     return ConversationChain(
         llm=llm,
         prompt=prompt,
-        memory=ConversationKGMemory(llm=llm, chat_memory=chat_memory),
+        memory=ConversationKGMemory(llm=llm, chat_memory=chat_memory)
     ).predict(message)
