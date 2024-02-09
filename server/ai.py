ae4a7793d5693b6694289fb7febaace7b67debe0
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-02-09 13:41:51 -0500

    stylefixes commit message

--- server/ai.py
+++ server/ai.py
@@ -3,6 +3,7 @@ This file contains the AI logic for the chatbot.
 It is responsible for generating responses to user messages. 
 """
 <<<<<<< HEAD
+<<<<<<< HEAD
 This file contains the AI logic for the chatbot. It is responsible for 
 generating responses to user messages.
 """
@@ -12,14 +13,17 @@ from langchain.prompts import PromptTemplate
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 =======
+=======
+from dotenv import load_dotenv
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationKGMemory
+from langchain.prompts.prompt import PromptTemplate
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_openai import OpenAI
+>>>>>>> 7089197 (stylefixes)
 
 >>>>>>> 51bd514 (added memory)
 import models
-from dotenv import load_dotenv
-from langchain.memory import ConversationKGMemory
-from langchain_openai import OpenAI
-from langchain.chains import ConversationChain
-from langchain.prompts.prompt import PromptTemplate
 
 <<<<<<< HEAD
 # Model too big for GPU
@@ -39,19 +43,19 @@ load_dotenv()
 
 llm = OpenAI()
 
-template = """The following is a friendly conversation between a human and an AI. 
-The AI is talkative and provides lots of specific details from its context. 
-If the AI does not know the answer to a question, it truthfully says it does not know. 
-The AI ONLY uses information contained in the "Relevant Information" section and does not hallucinate. 
-Relevant Information: {history} 
-Conversation: Human: {input} 
+template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. 
+If the AI does not know the answer to a question, it truthfully says it does not know. The AI ONLY uses information contained in the "Relevant Information" section and does not hallucinate.
+
+Relevant Information:
+
+{history}
+
+Conversation:
+Human: {input}
 AI:"""
 
-prompt = PromptTemplate(input_variables=["history", "input"], template=template)
 
-conversation = ConversationChain(
-    llm=llm, prompt=prompt, memory=ConversationKGMemory(llm=llm)
-)
+prompt = PromptTemplate(input_variables=["history", "input"], template=TEMPLATE)
 
 
 def ai_message(chat_id, message):
@@ -61,4 +65,11 @@ def ai_message(chat_id, message):
     @return: the response from the AI
     """
     session = models.Chats.query.get(chat_id)
-    return conversation.predict(message)
+
+    chat_memory = BaseChatMessageHistory()
+
+    return ConversationChain(
+        llm=llm,
+        prompt=prompt,
+        memory=ConversationKGMemory(llm=llm, chat_memory=chat_memory),
+    ).predict(message)
