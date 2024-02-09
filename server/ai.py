dea0416cbf2b4d977f486b0246e91e277e9cdd4c
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-02-09 14:39:01 -0500

    donezo

--- server/ai.py
+++ server/ai.py
@@ -4,33 +4,35 @@ It is responsible for generating responses to user messages.
 """
 from dotenv import load_dotenv
 from langchain.chains import ConversationChain
-from langchain.memory import ConversationKGMemory
+from langchain.memory import ConversationKGMemory, ChatMessageHistory
 from langchain.prompts.prompt import PromptTemplate
-from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_openai import OpenAI
 
 from models import Messages
 
+# Load in template
+with open("data/template.txt", encoding="utf-8") as file:
+    template = file.read()
+
+# Setup LLM
 load_dotenv()
 llm = OpenAI()
 prompt = PromptTemplate(
-    input_variables=["history", "input"], 
-    template=open("data/template.txt").read()
+    input_variables=["history", "input"],
+    template=template
 )
 
-def ai_message(chat_id, message):
+def ai_message(chat_id, human_message):
     """
     @chat_id: the id of the chat
     @message: the message to the AI
     @return: the response from the AI
     """
     # Collect message history
-    messages = Messages.query.filter_by(chat=chat_id)
-                             .order_by(Messages.time)
-                             .all()
+    messages = Messages.query.filter_by(chat=chat_id).order_by(Messages.time).all()
 
     # Transform data for langchain useage
-    chat_memory = BaseChatMessageHistory()
+    chat_memory = ChatMessageHistory()
     for message in messages:
         if message.chat_type == "AI":
             chat_memory.add_ai_message(message.text)
@@ -42,4 +44,4 @@ def ai_message(chat_id, message):
         llm=llm,
         prompt=prompt,
         memory=ConversationKGMemory(llm=llm, chat_memory=chat_memory)
-    ).predict(message)
+    ).predict(human_message)
