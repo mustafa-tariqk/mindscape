47e8d8cfd9ee4d3dffc45b695ae7ae53fd18b65b
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-03-04 16:09:09 -0500

    updating docs, fixing docs script

--- server/docpy.sh
+++ server/docpy.sh
@@ -1,6 +1,5 @@
 # This script must be run in the server directory.
-cd server
 mkdir -p docs
 rm -rf docs/*
-find . -name '*.py' -not -path "./venv/*" -exec python3 -m pydoc -w {} \;
+find . -name '*.py' -not -path "./venv/*" -exec python -m pydoc -w {} \;
 mv *.html docs
