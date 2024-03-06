c9f64aab437db729c4de8e58827c98c59fe8e3ec
ComeOnConMan <killingbeck14@gmail.com>
2024-03-06 15:09:38 -0500

    Updated legal agreement

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import AdminHome from "./pages/AdminHome.jsx";
+import Complete from "./pages/Complete.jsx";
 import "./style.scss"
-
 
 function App() {
   return (
-    <Home />
+    <Login />
   );
 }
 
