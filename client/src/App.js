57166b93d4366f434e5df1d4314e7258cc2c2518
ComeOnConMan <killingbeck14@gmail.com>
2024-03-08 23:58:43 -0500

    Did some moose fuckery

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,26 @@ import AdminHome from "./pages/AdminHome.jsx";
 import Complete from "./pages/Complete.jsx";
 import "./style.scss"
 
+const SERVER_URL = process.env.SERVER_URL;
+
 function App() {
+
+  //Login check here
+  fetch(SERVER_URL + '/api/user', {
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.hasOwnProperty('error')) {
+        window.location.href = SERVER_URL + '/login';
+      }
+    });
+
   return (
     <Home />
   );
