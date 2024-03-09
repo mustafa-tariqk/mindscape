9b8b70dbf988f1d2cf59bd6cbefe96d62dea76b6
ComeOnConMan <killingbeck14@gmail.com>
2024-03-09 00:22:35 -0500

    Mooses fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
-import AdminHome from "./pages/AdminHome.jsx";
-import Complete from "./pages/Complete.jsx";
 import "./style.scss"
-
-const SERVER_URL = process.env.SERVER_URL;
 
 function App() {
   return (
