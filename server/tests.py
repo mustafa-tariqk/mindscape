3defeaaec0ab5cd398d0de3ee86da445af70baaf
mustafa-tariqk <mustafa.tariqk@icloud.com>
2024-03-11 13:51:28 -0400

    test added for troll capture

--- server/tests.py
+++ server/tests.py
@@ -66,6 +66,15 @@ def test_flag_chat(fake_client: FlaskClient):
     data = response.get_json()
     assert 'status' in data and data['status'] == 'flagged'
 
+def test_get_trolls(fake_client: FlaskClient):
+    """
+    Test the get trolls route
+    """
+    response = fake_client.get('/get_trolls')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, dict)
+
 
 def test_get_all_chats(fake_client: FlaskClient):
     """
