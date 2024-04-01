cc95a96f30cbbfbc927c21486a55652bbb28294f
Khoa Nguyen <dangkhoa81122@gmail.com>
2024-04-01 01:24:37 -0400

    Let's see if this works

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,34 +1,38 @@
 import React, { useState } from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from 'react-router-dom'
+
+import Complete from './Complete.jsx';
 import Chat from '../components/Chat.jsx'
 import Topbar from '../components/Topbar.jsx'
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Link
-} from 'react-router-dom'
-import Complete from './Complete.jsx';
+import Login from './Login.jsx'
+
 
 function Home() {
 
     //Chat id passing
     const [chatId, setChatId] = useState(null);
+    const [userId, setUserId] = useState(null);
 
     return (
-        <div className='home'>
-            <div className='topbar'>
-                <Topbar/>
-            </div>
-            <Router>
+        <Router>
+            <div className='home'>
+                <div className='topbar'>
+                    <Topbar />
+                </div>
                 <div className='container'>
                     <Routes>
-                        <Route path="/" element={<Chat chatId={chatId} setChatId={setChatId}/>} />
+                        <Route path="/" element={<Chat chatId={chatId} userId={userId} setChatId={setChatId} setUserId={setUserId}/>}/>
                         <Route path="/complete" element={<Complete chatId={chatId} />} />
+                        <Route path="/login" element={<Login />} />
                         <Route path="/logout" element={<handleLogout />} />
                     </Routes>
                 </div>
-            </Router>
-        </div>
+            </div>
+        </Router>
     )
 }
 
